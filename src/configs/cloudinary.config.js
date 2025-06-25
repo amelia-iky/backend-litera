567e9744066cc374ebedc02fce7946e0282b413a
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-06-25 15:28:35 +0800

    feat: add Cloudinary configuration for image uploads

---
+++ src/configs/cloudinary.config.js
@@ -0,0 +1,26 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+require('dotenv').config();
+
+// Konfigurasi Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Konfigurasi storage multer
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'profileImages',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload };
