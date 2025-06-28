cfb67440cb50c346def482e588a3b4ff328f2906
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-06-28 18:37:15 +0800

    refactor: improve comments for clarity in book.routes.js

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -2,9 +2,14 @@ const router = require('express').Router();
 const book = require('../controllers/book.controller');
 const { authentication } = require('../middleware/authentication.middleware');
 
-// Favorite
+// Favorite Books
 router.post('/favorite', authentication(), book.addFavorite);
 router.get('/favorite', authentication(), book.getFavorites);
 router.delete('/favorite/:id', authentication(), book.deleteFavorite);
+
+// Saved Books
+router.post('/save', authentication(), book.addSave);
+router.get('/save', authentication(), book.getSave);
+router.delete('/save/:id', authentication(), book.deleteSave);
 
 module.exports = router;
