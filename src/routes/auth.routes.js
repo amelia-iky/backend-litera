892ff5d36c305abaa487c1c61f12aa336de7a255
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-06-28 14:58:19 +0800

    refactor: remove unused User model import from auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const auth = require('../controllers/auth.controller');
 const { authentication } = require('../middleware/authentication.middleware');
-const User = require('../models/user.model');
 
 // Signup
 router.post('/signup', auth.signup);
