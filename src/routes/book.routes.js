58dc897a245548e6d6cd52cfa24695ff5e966e5f
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-06-28 17:24:34 +0800

    feat: add delete favorite endpoint to book routes

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -5,5 +5,6 @@ const { authentication } = require('../middleware/authentication.middleware');
 // Favorite
 router.post('/favorite', authentication(), book.addFavorite);
 router.get('/favorite', authentication(), book.getFavorites);
+router.delete('/favorite/:id', authentication(), book.deleteFavorite);
 
 module.exports = router;
